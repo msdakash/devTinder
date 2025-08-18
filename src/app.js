1acfb03c33b02ae02e0db88b0dec85f27c7603ce
Akash Shaw <akashshaw730@gmail.com>
2025-08-18 21:24:37 +0530

    error handling , middlewares , brahmastra error handling

--- src/app.js
+++ src/app.js
@@ -1,19 +1,92 @@
 const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({
-    firstName: "Akash",
-    lastName: "Shaw",
-  });
-});
-app.post("/user", (req, res) => {
-  res.send("Data saved successfully");
+app.use("/admin", adminAuth);
+
+app.get("/user/userData", userAuth, (req, res, next) => {
+  res.send("User data");
 });
 
-app.delete("/user", (req, res) => {
-  res.send("Data deleted successfully");
+app.get("/user/login", (req, res, next) => {
+  res.send("Logged In");
 });
+
+app.get("/admin/getAllData", (req, res, next) => {
+  return res.send("All data got");
+});
+
+app.get("/admin/deleteData", (req, res, next) => {
+  return res.send("Delete data succesfully");
+});
+
+app.get("/error", (req, res, next) => {
+  // Always use try catch
+  try {
+    throw new Error("ABCBCBCK");
+  } catch (err) {
+    res.status(500).send("Please contact admin");
+  }
+});
+
+// One way to handle (Brahmastra)
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+// app.use(
+//   "/user",
+//   [
+//     (req, res, next) => {
+//       // res.send("Test");
+//       next();
+//     },
+//     (req, res, next) => {
+//       // res.send("Test2");
+//       next();
+//     },
+//   ],
+//   (req, res, next) => {
+//     // res.send("Test3");
+//     next();
+//   }
+// );
+
+// app.get(
+//   "/users",
+//   [
+//     (req, res, next) => {
+//       // res.send("Test");
+//       next();
+//     },
+//     (req, res, next) => {
+//       // res.send("Test2");
+//       next();
+//     },
+//   ],
+//   (req, res, next) => {
+//     res.send("Test3");
+//     next();
+//   }
+// );
+
+// app.get("/user/:id/:name", (req, res) => {
+//   console.log(req.params);
+//   res.send({
+//     firstName: "Akash",
+//     lastName: "Shaw",
+//   });
+// });
+
+// app.post("/user", (req, res) => {
+//   res.send("Data saved successfully");
+// });
+
+// app.delete("/user", (req, res) => {
+//   res.send("Data deleted successfully");
+// });
 
 app.listen(3000, () => {
   console.log(`Running on port 3000`);
---
+++ src/middlewares/auth.js
@@ -0,0 +1,24 @@
+const adminAuth = (req, res, next) => {
+  const token = "xyz";
+  const isAuthorized = token === "xyz";
+  if (!isAuthorized) {
+    res.status(401).send("Unauthorized");
+  } else {
+    next();
+  }
+};
+
+const userAuth = (req, res, next) => {
+  const token = "xyz";
+  const isAuthorized = token === "xyz";
+  if (!isAuthorized) {
+    res.status(401).send("Unauthorized");
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  adminAuth,
+  userAuth,
+};
